ebf49b56c2b8f9781980d465a1dcdb52369f3963
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-04-17 10:29:59 +0200

    push

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //Enklare Webbtjänst
 
+//1. Skapar endast en respons till webbläsaren i form av text
 //Vi måste använda http modul
-const http = require('http')
+/* const http = require('http')
 
 //Vi ska skapa en server i node med funktionen createServer
 //I denna ska det finnas en callback funktion som kan hantera våra respons, requests och göra något med detta
@@ -9,6 +10,28 @@ const http = require('http')
 const app = http.createServer((request, response) => {
     response.write('Hej allihopa nu får vi svar från vår egna server')
     response.end()
+})
+
+app.listen({ port: 3000}, () => {
+    console.log('Server startad på localhost:3000')
+}) */
+
+//Skapar en respons som "tittar efter" vilken address (path) besökaren är på och ger olika
+//svar tillbaka beroende på vilken adress de valt, detta görs med en if sats eller exempelvis en switch
+const http = require('http')
+
+//Vi ska skapa en server i node med funktionen createServer
+//I denna ska det finnas en callback funktion som kan hantera våra respons, requests och göra något med detta
+//request, response kanj även skrivas såhär: res, req
+const app = http.createServer((request, response) => {
+   if(request.url === '/'){
+    response.write('Du är på hem adressen')
+   }
+   else {
+    response.write('Annan adress')
+   }
+   response.end()
+
 })
 
 app.listen({ port: 3000}, () => {
