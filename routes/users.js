e4b39121b42be1ff37237367c476c9def6568237
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-04-18 11:41:17 +0200

    push

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,6 @@ const router = express.Router()
 //Obs:tänk på att alla dynamiska adresser ska ligga längst, de statiska adresserna
 //ska ligga överst
 
-
-
-
 router.get('/:id', (req, res) => {
     console.log(req.user)
     res.send(`Se användare med id ${req.params.id}`)
