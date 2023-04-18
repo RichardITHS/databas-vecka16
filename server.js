0fc5ece040d073801320799377f83f8d4ce73c9d
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-04-18 10:52:39 +0200

    push

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express')
 const app = express()
 
 //Nedan gör vi en enkel GET request
-app.get('/', (request, response) => {
+/* app.get('/', (request, response) => {
     response.send('Hello World!')
   })
 
@@ -22,6 +22,18 @@ app.get('/', (request, response) => {
     //Sending json back
     response.send({ greeting: 'Hello World!' })
   })
+ */
+
+//Kunna visa en fil beroende på vilken adress man går till
+//Då kan vi arbeta med EJS paketet för at åstadkomma detta
+
+//Implementera ejs
+app.set("view engine", "ejs")
+
+//Skapar en get
+app.get('/', (req, res) => {
+    res.render('index', {name:'Richard'})
+})
 
 app.listen(3000, () => {
     console.log('Server startad på port 3000')
